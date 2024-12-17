f69969d799e156ae0bdee7222d4971c2a7080b57
Lucas Assis <l.assis@nextage.com.br>
2024-12-17 17:39:27 -0300

    Melhoria na validação da criação de placas

--- Avaliacao.Application/Veiculo/Commands/CadastrarVeiculo/Validators/CadastrarVeiculoValidator.cs
+++ Avaliacao.Application/Veiculo/Commands/CadastrarVeiculo/Validators/CadastrarVeiculoValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
 
 namespace Avaliacao.Application.Veiculo.Commands.CadastrarVeiculo.Validators
 {
@@ -9,23 +10,24 @@ namespace Avaliacao.Application.Veiculo.Commands.CadastrarVeiculo.Validators
             RuleFor(c => c.Placa)
                 .NotEmpty()
                 .NotNull()
+                .Matches(new Regex(@"^[A-Z]{3}-\d{4}$|^[A-Z]{3}\d{1}[A-Z]{1}\d{2}$"))
                 .WithMessage("A placa informada é inválida.")
                 .WithName("Placa");
 
             RuleFor(c => c.Cor)
                 .NotEmpty()
                 .NotNull()
-                .WithMessage("A cor não deve ser nulo.");
+                .WithMessage("A cor não pode ser nula.");
 
             RuleFor(c => c.Modelo)
                 .NotEmpty()
                 .NotNull()
-                .WithMessage("O modelo não deve ser nulo.");
+                .WithMessage("O modelo não pode ser nulo.");
 
             RuleFor(c => c.Marca)
                 .NotEmpty()
                 .NotNull()
-                .WithMessage("A marca não deve ser nula.");
+                .WithMessage("A marca não pode ser nula.");
         }
     }
 }
